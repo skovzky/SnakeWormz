1650270480c7394340e4e0cfe9d2cbe1011ebe0e
Vladimir <rcheemann@hotmail.com>
2013-03-21 12:56:28 -0500

    added a cool head

--- le snakes.rkt
+++ le snakes.rkt
@@ -1,10 +1,15 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |le snakes|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |le snakes|) (read-case-sensitive #t) (teachpacks ((lib "image.ss" "teachpack" "2htdp") (lib "universe.ss" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.ss" "teachpack" "2htdp") (lib "universe.ss" "teachpack" "2htdp")))))
 ;le physical constants
 (define HEIGHT 19)
 (define SCALE 30)
 
-;le cirlce
-(define HEAD (circle (/ SCALE 2) outline red))
-(define BODY (circle (/ SCALE 2) solid purple))
+;le SNAKE
+(define HEAD 
+  (underlay/offset (circle 40 "solid" "gray")
+                   0 -10
+                   (underlay/offset (circle 10 "solid" "navy")
+                                   -30 0
+                                   (circle 10 "solid" "navy"))))
+(define BODY (circle (/ SCALE 2) "solid" "purple"))
