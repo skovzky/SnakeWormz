62b00ae73c81fe6c0fce0b9f5f2266d5a92d72da
Vladimir <rcheemann@hotmail.com>
2013-04-02 12:56:15 -0500

    got a lot done
    yayuhz

--- le snakes.rkt
+++ le snakes.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname |le snakes|) (read-case-sensitive #t) (teachpacks ((lib "image.ss" "teachpack" "2htdp") (lib "universe.ss" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.ss" "teachpack" "2htdp") (lib "universe.ss" "teachpack" "2htdp")))))
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname |le snakes|) (read-case-sensitive #t) (teachpacks ((lib "image.ss" "teachpack" "2htdp") (lib "batch-io.ss" "teachpack" "2htdp") (lib "universe.ss" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.ss" "teachpack" "2htdp") (lib "batch-io.ss" "teachpack" "2htdp") (lib "universe.ss" "teachpack" "2htdp")))))
 ;le physical constants
 (define HEIGHT 19)
 (define SCALE 30)
@@ -23,6 +23,14 @@
 (define BACKGROUND (empty-scene (* 10 SCALE) (* 10 HEIGHT) "brown"))
 
 
+;World
+;game-state --> image
+;image to be placed into a window
+(define (world-render gs)
+  (let*([pos (game-pos gs)]
+        [x (posn-x pos)]
+        [y (posn-y pos)])
+    (place-image
 ;movement
 ;data definitions
 ;direction is current direction of travel for the worm, and is eiter
@@ -39,13 +47,19 @@
           [dir  (game-dir gs)]
           [x (posn-x pos)]
           [y (posn-y pos)])
-    (cond [(= dir 0) (make-game (make-posn x (-y 1)) dir)]
-          [(= dir 1) ...]
-          [(= dir 2) ...]
-          [(= dir 3) ...])))
+    (cond [(= dir 0) (make-game (make-posn x (- y 1)) dir)]
+          [(= dir 1) (make-game (make-posn (+ x 1) y) dir)]
+          [(= dir 2) (make-game (make-posn x (+ y 1)) dir)]
+          [(= dir 3) (make-game (make-posn (- x 1) y) dir)])))
 
 ;Game Keypress -> Game
-(define (command gs key)...)
+(define (command gs key)
+  (let*([pos(game-pos gs)])
+  (cond
+    [(key=? key "up") (make-game pos 0)]
+    [(key=? key "right") (make-game pos 1)]
+    [(key=? key "down") (make-game pos 2)]
+    [(key=? key "left") (make-game pos 3)])))
 
 ;worm-ness (making the body be a body that follows)
 
@@ -53,3 +67,12 @@
 
 
 ;food placement
+
+
+
+;CREATE LE UNIVERSE
+(big-bang GAME
+          (on-tick move)
+          (on-key command)
+          (to-draw world-render)
+          )
